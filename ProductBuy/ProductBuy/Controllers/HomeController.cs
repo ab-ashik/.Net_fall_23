fcdfe297ad7e0c0e329f4079b705975c329e62d8
Abdullah Ashik <abdullahashik2001@gmail.com>
2023-11-27 00:17:02 +0600

    first commit

---
+++ ProductBuy/ProductBuy/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ProductBuy.EntityF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductBuy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var db = new DB_TableEntities3();
+            var data = db.Products.ToList();
+
+
+            return View(data);
+        }
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
